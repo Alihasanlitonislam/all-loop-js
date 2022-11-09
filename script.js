6419cb7e0d2e179c16fb9d0fc9b9e8e32ac21b16
AliAliAliAli <alihasan404439@gmail.com>
2022-11-09 21:52:00 +0600

    i am learn in progrem in javascript

--- script.js
+++ script.js
@@ -15,3 +15,34 @@ let numberin = [5, 10, 11, 15, 17, 20, 21, 24, 28];
 for (let a in number) {
     console.log(`i am learn in progrem in javascript ${number[a]}`)
 }
+//for of
+let aliHasan = ["adom islam", "tusher islam", "roki islam", "masaddek islam", "julfikar islam", "julfikar rhaman jim"]
+for (let m of aliHasan) {
+    console.log(m)
+}
+let alihasan = ["adom islam", "tusher islam", "roki islam", "masaddek islam", "julfikar islam", "julfikar rhaman jim"]
+for (let m of alihasan) {
+    console.log(`welcome ${[m]}`)
+}
+// while loop
+let whileLoop = 5;
+while (whileLoop <= 15) {
+    console.log(whileLoop);
+    whileLoop++
+}
+let whileL = 5;
+while (whileL <= 15) {
+    console.log(`i am students ${whileL}`);
+    whileL++
+}
+//do - while loop
+let doloop = 0;
+do {
+    console.log(doloop)
+    doloop++
+} while (doloop <= 10)
+let dol = 0;
+do {
+    console.log(`Bangladesh is a small contry ${dol}`)
+    dol++
+} while (dol <= 8)
