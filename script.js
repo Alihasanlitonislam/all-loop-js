cb3969bb60b17cc2540f631cce417c16d39bd8a7
AliAliAliAli <alihasan404439@gmail.com>
2022-11-09 21:03:56 +0600

    loop add to github md ali hasan

---
+++ script.js
@@ -0,0 +1,17 @@
+//for loop 
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+const ali = ["ali hasan", "shoriful islam", "alamin islam", "milon islam"]
+for (let a = 0; a < ali.length; a++) {
+    console.log(`hi ${ali[a]} welcome to bangladesh`)
+}
+//for in loop
+let number = [5, 10, 11, 15, 17, 20, 21, 24, 28];
+for (let a in number) {
+    console.log(number[a])
+}
+let numberin = [5, 10, 11, 15, 17, 20, 21, 24, 28];
+for (let a in number) {
+    console.log(`i am learn in progrem in javascript ${number[a]}`)
+}
